dc1567cd1eab28795cbe2e4c90bed6bd2c7e218f
Andreas Bonell <andreas.bonell@analogfolk.com>
2014-10-01 14:59:33 -0400

    Initial commit, including gulpfile and source directories.

---
+++ README.md
@@ -0,0 +1,13 @@
+# AnalogFolk NY Boilerplate
+
+## Requirements
+
+[Node.js](https://www.nodejs.org)
+
+### Uses
+
+[Gulp](https://github.com/gulpjs/gulp) as the build tool
+
+## Getting started
+
+	npm install
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+// Require dependencies
+var gulp        = require('gulp');
+var changed     = require('gulp-changed'); // https://github.com/sindresorhus/gulp-changed
+
+// Project constants
+// Change as suitable
+var SRC  = 'src';
+var DEST = 'dist';
+
+// Tasks
+
+// Task to run during development
+gulp.task('dev', function() {
+
+});
+
+// Task to run for distribution
+gulp.task('dist', function() {
+
+});
+
+// Watch for changes and build for development
+gulp.task('watch', function() {
+
+});
